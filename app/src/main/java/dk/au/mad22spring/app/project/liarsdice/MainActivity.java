6a6e348fe2e9416e91d2b5bfc151626723a857cb
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2022-04-18 15:05:35 +0200

    init project

---
+++ app/src/main/java/dk/au/mad22spring/app/project/liarsdice/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.au.mad22spring.app.project.liarsdice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
