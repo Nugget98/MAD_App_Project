ead527ef887e355a27c82c4dce151a5caa18fa7e
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2022-04-28 15:40:45 +0200

    WIP profile

--- app/src/main/java/dk/au/mad22spring/app/project/liarsdice/Activities/HomeActivity.java
+++ app/src/main/java/dk/au/mad22spring/app/project/liarsdice/Activities/HomeActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import dk.au.mad22spring.app.project.liarsdice.R;
+import dk.au.mad22spring.app.project.liarsdice.Utilities.FirestoreUtil;
+import dk.au.mad22spring.app.project.liarsdice.Utilities.GoogleAuthenticationUtil;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FirestoreUtil firestoreUtil = new FirestoreUtil();
+        GoogleAuthenticationUtil googleAuthenticationUtil = new GoogleAuthenticationUtil();
+        firestoreUtil.updateUser(googleAuthenticationUtil.getSignedInUserUID(), "Penis", 5, 0);
+        firestoreUtil.readUser(googleAuthenticationUtil.getSignedInUserUID());
     }
 }
