2c0d84b0e4aa8b7ce1ab53f8c74e3e87d9ccd785
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2022-04-28 13:59:19 +0200

    Added room activity

---
+++ app/src/main/java/dk/au/mad22spring/app/project/liarsdice/Activities/RoomActivity.java
@@ -0,0 +1,16 @@
+package dk.au.mad22spring.app.project.liarsdice.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dk.au.mad22spring.app.project.liarsdice.R;
+
+public class RoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+    }
+}
