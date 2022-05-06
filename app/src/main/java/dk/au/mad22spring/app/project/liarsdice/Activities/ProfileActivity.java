f503545370970f785696a2bd073da17b1a00c3f4
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2022-05-06 17:19:42 +0200

    WIP profile

--- app/src/main/java/dk/au/mad22spring/app/project/liarsdice/Activities/ProfileActivity.java
+++ app/src/main/java/dk/au/mad22spring/app/project/liarsdice/Activities/ProfileActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,10 +20,19 @@ import dk.au.mad22spring.app.project.liarsdice.Utilities.User;
 
 public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "FirestoreUtil: ";
+
+    private TextView txtWins;
+    private TextView txtLoses;
+    private TextView txtTotalGames;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        txtWins = findViewById(R.id.txtDisplayWins);
+        txtLoses = findViewById(R.id.txtDisplayLoses);
+        txtTotalGames = findViewById(R.id.txtDisplayTotalGames);
 
         FirestoreUtil firestoreUtil = new FirestoreUtil();
         GoogleAuthenticationUtil googleAuthenticationUtil = new GoogleAuthenticationUtil();
@@ -35,8 +45,12 @@ public class ProfileActivity extends AppCompatActivity {
                             DocumentSnapshot document = task.getResult();
                             if (document.exists()) {
                                 Map<String, Object> data = document.getData();
-                                Log.d(TAG, "DocumentSnapshot data: " + data.get("Displayname"));
+                                Log.d(TAG, "DocumentSnapshot data: " + data.get("Wins"));
                                 user.setDisplayname(data.get("Displayname").toString());
+                                user.Wins = data.get("Wins").toString();
+                                user.Loses = data.get("Loses").toString();
+                                user.TotalGames = String.valueOf(Integer.parseInt(user.Wins) - Integer.parseInt(user.Loses));
+                                Log.d(TAG, "User wins: " + user.Wins);
                                 updateUI(user);
                             } else {
                                 Log.d(TAG, "No such document");
@@ -46,6 +60,11 @@ public class ProfileActivity extends AppCompatActivity {
                         }
                     }
                 });
+        //user.TotalGames = String.valueOf(Integer.parseInt(user.Wins) - Integer.parseInt( user.Loses));
+
+        txtWins.setText("" + user.Wins);
+        txtLoses.setText(user.Loses);
+        //txtTotalGames.setText(user.TotalGames);
 
 
     }
